0f940a6abf80d21c381d437ba67755ee3db0fdf5
Rekurt <sabrlitet@gmail.com>
2025-10-23 13:41:00 +0300

    comleted layout header and adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('../components/home/home').then(m => m.Home)
+    },
+    {
+        path: 'about',
+        loadComponent: () => import('../components/about/about').then(m => m.About)
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('../components/contacts/contacts').then(m => m.Contacts)
+    },
+    {
+        path: 'catalog',
+        loadComponent: () => import('../components/catalog/catalog').then(m => m.Catalog)
+    },
+    {
+        path: 'admin',
+        loadComponent: () => import('../components/admin/admin').then(m => m.Admin),
+    },
+    {
+        path: 'cart',
+        loadComponent: () => import('../components/cart/cart').then(m => m.Cart)
+    }
+];
